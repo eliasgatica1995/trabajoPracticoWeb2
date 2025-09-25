f951042021c9e04e70cd702d4b724f771e315ed0
Sebastian Martinez <sebaamartinez54@gmail.com>
2025-09-25 15:34:29 -0300

    cambio de color a los Tags

--- componentes/fichaItem.js
+++ componentes/fichaItem.js
@@ -141,8 +141,8 @@ export class ProductoFicha extends LitElement {
                     ${this.tags.map((tag) => {
                       const styles = {
                         Promoción: "bg-red-100 text-red-800",
-                        Orgánico: "bg-blue-100 text-blue-800",
-                        "Producto local": "bg-yellow-100 text-yellow-800",
+                        Orgánico: "bg-green-100 text-green-800",
+                        "Producto local": "bg-blue-100 text-blue-800",
                       };
                       const style =
                         styles[tag.title] || "bg-gray-100 text-gray-800";
